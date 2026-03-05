da5cbaf03c14d020c3c9a14904aa66666af36f84
berkaygokcee <berkaygokce1065@gmail.com>
2026-03-05 02:44:35 -0800

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,36 @@
+while True:
+    n = int(input("Please enter a number between 10 and 100: "))
+    if 10 <= n <= 100:
+        break
+    else:
+        print("Invalid input. Please enter a number between 10 and 100:")
+
+fizz_count = 0
+buzz_count = 0
+fizzbuzz_count = 0
+
+for i in range(1, n + 1):
+
+    if i % 7 == 0:
+        print(f"({i} is skipped)")
+        continue
+
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+        fizzbuzz_count += 1
+
+    elif i % 3 == 0:
+        print("Fizz")
+        fizz_count += 1
+
+    elif i % 5 == 0:
+        print("Buzz")
+        buzz_count += 1
+
+    else:
+        print(i)
+
+print("--- Summary ---")
+print("Fizz count :", fizz_count)
+print("Buzz count :", buzz_count)
+print("FizzBuzz count:", fizzbuzz_count)
